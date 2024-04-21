c8bb8bee3e8639d6fed7cb9cb9a7c6ac59d1cfd4
Carmen-Lewis <crmnlewis@gmail.com>
2024-04-21 18:42:21 +0200

    adding the header items

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,21 @@
 window.onload = function() {
+    console.log("Page loaded");
     setTimeout(function() {
         var rightSection = document.querySelector('.right-section');
         var headerBanner = document.querySelector('.header-banner');
         headerBanner.style.display = 'flex'; // Show the header banner
+        console.log("Header banner displayed");
         setTimeout(function() {
             rightSection.classList.add('expand');
+            console.log("Right section expanded");
         }, 3000); // Delay the expand animation by 3000ms (3 seconds) after the landing animation
     }, 3000); // Delay for 3000ms (3 seconds) before starting the animations
+
+    const menuIcon = document.querySelector('.menu-icon');
+    const menuItems = document.querySelector('.menu-items');
+
+    menuIcon.addEventListener('click', function() {
+        menuItems.classList.toggle('open');
+        menuIcon.classList.toggle('flip'); // Toggle the flip class
+    });
 };
