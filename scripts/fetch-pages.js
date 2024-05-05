ff198712d4c0927b94744c79c38d0b3222bca22d
Carmen-EXPLORE <crmnlewis@gmail.com>
2024-05-05 12:40:08 +0200

    adding more pages and working on navigation

--- scripts/fetch-pages.js
+++ scripts/fetch-pages.js
@@ -5,25 +5,45 @@ async function fetchFilesFromFolder(folderPath) {
     return data;
 }
 
-// Function to generate card elements based on file names
-function generateCardsFromFiles(files) {
+// Function to fetch metadata from HTML content
+function extractMetadataFromHTML(htmlContent) {
+    const doc = new DOMParser().parseFromString(htmlContent, 'text/html');
+    const title = doc.querySelector('title').textContent || '';
+    const description = doc.querySelector('meta[name="description"]').getAttribute('content') || '';
+    const keywords = doc.querySelector('meta[name="keywords"]').getAttribute('content') || '';
+    const author = doc.querySelector('meta[name="author"]').getAttribute('content') || '';
+    const date = doc.querySelector('meta[name="date"]').getAttribute('content') || '';
+    const type = doc.querySelector('meta[name="type"]').getAttribute('content') || '';
+    return { title, description, keywords, author, date, type };
+}
+
+// Function to generate card elements based on file names and metadata
+async function generateCardsFromFiles(files) {
     const container = document.querySelector('.card-container');
 
-    files.forEach(file => {
+    for (const file of files) {
         if (file.type === 'file' && file.name.endsWith('.html')) {
             const card = document.createElement('div');
             card.classList.add('card');
 
-            const fileName = file.name.replace('.html', '');
+            const response = await fetch(file.download_url);
+            const htmlContent = await response.text();
+            const { title, description } = extractMetadataFromHTML(htmlContent);
+
+            const cardText = "card-preview-text";
+            const typeStyle = "type";
+            
             card.innerHTML = `
                 <a href="${folderPath}/${file.name}">
-                    <h2>${file.title}</h2>
+                    <span class="${typeStyle}">${type}</span>${title}
+                    <h2>${title}</h2>
+                    <p>${description}</p>
                 </a>
             `;
 
             container.appendChild(card);
         }
-    });
+    }
 }
 
 // Specify the folder path in your GitHub repository
