554ee67ab0ce81a98b615e290812f8a33be579f1
Carmen-EXPLORE <crmnlewis@gmail.com>
2024-05-06 22:20:59 +0200

    moving away from complex flex within flex to grid

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,11 @@
 window.onload = function() {
 
-    const fullPage = document.querySelector('.full-page');
-    fullPage.style.height = `${window.innerHeight}px`;
-
     const menuIcon = document.querySelector('.menu-icon');
     const menuItems = document.querySelector('.menu-items');
 
     menuIcon.addEventListener('click', function() {
         menuItems.classList.toggle('open');
-        menuIcon.classList.toggle('flip'); // Toggle the flip class
+        menuIcon.classList.toggle('flip');
     });
 
     const prev = document.getElementById('prev-btn')
@@ -31,22 +28,6 @@ window.onload = function() {
         });
     }
 
-    const searchIcon = document.querySelector('.search-icon');
-    const searchBar = document.getElementById('search-bar');
-
-    // searchBar.style.display = 'none';
-
-    // searchIcon.addEventListener('click', function() {
-        // Toggle the visibility of the search bar
-    //    if (searchBar.style.display === 'none') {
-    //        searchBar.style.display = 'block';
-    //        searchIcon.classList.add('search-selected');
-    //    } else {
-    //        searchBar.style.display = 'none';
-    //        searchIcon.classList.remove('search-selected');
-    //    }
-    //});
-
     // Fetch the JSON data
     fetch('/data/tags.json')
     .then(response => response.json())
@@ -63,19 +44,4 @@ window.onload = function() {
     });
     })
     .catch(error => console.error('Error fetching tags:', error));
-
-    const tagsIcon = document.querySelector('.tags-icon');
-    const tagCloud = document.querySelector('.tag-cloud');
-
-    tagCloud.style.display = 'none';
-
-    tagsIcon.addEventListener('click', function() {
-        if (tagCloud.style.display === 'none') {
-            tagCloud.style.display = 'flex';
-            tagsIcon.classList.add('tags-selected');
-        } else {
-            tagCloud.style.display = 'none';
-            tagsIcon.classList.remove('tags-selected');
-        }
-    });
 };
